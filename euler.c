fa2c7f2bf1ab60c92112d3458bb5e2c850fe3665
Nacho Granados <ignaciogramar@gmail.com>
2022-09-28 13:22:02 -0600

    Adding euler

---
+++ euler.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <time.h>
+#include <omp.h>
+
+int main() {
+
+    double e;
+
+    int i;
+
+    //int n = 165;
+    //int n = 330;
+    //int n = 495;
+    //int n = 660;
+    //int n = 825;
+    int n = 1000;
+    
+    double a[1000];
+
+    double start_time;
+    double end_time;
+
+    a[0] = 1.0;
+
+    start_time = omp_get_wtime();
+
+    #pragma omp parallel for private(i)
+    for (i = 1; i < n; i++) {
+
+        a[i] = a[i-1] / i;
+
+    }
+
+    e = 1.0;
+
+    #pragma omp parallel for private(i)
+    for (i = n - 1; i > 0; i--) {
+
+        e += a[i];
+
+    }
+
+    end_time = omp_get_wtime();
+
+    printf("Euler constant e = %.16lf\n", e);
+
+    printf ("Time: %f\n", end_time - start_time);  
+    
+    return 0;
+
+}
